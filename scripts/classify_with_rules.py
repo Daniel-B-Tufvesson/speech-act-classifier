9b1db181dfb790d8fc97a02d1182d1f42f668b32
danieltufvesson <135624160+Daniel-B-Tufvesson@users.noreply.github.com>
2024-03-11 15:23:24 +0100

    Improve the evaluation function with sklearn.

--- scripts/classify_with_rules.py
+++ scripts/classify_with_rules.py
@@ -6,9 +6,11 @@ import speechact.classifier.rulebased as rb
 import speechact.classifier.base as b
 import speechact.evaluation as evaluation
 import speechact.corpus as corp
+import speechact.annotate as annotate
 
 if __name__ == '__main__':
     corpus = corp.Corpus('data/annotated data/dev-set.connlu.bz2')
+    labels = annotate.SpeechActLabels.get_labels()
 
     print()
 
@@ -17,17 +19,17 @@ if __name__ == '__main__':
     most_frequent.train(corpora=[corpus])
     print(f'Most frequenct class is "{most_frequent.most_common}"')
     print('Baseline results:')
-    evaluation.evaluate(corpus, most_frequent)
+    evaluation.evaluate(corpus, most_frequent, labels)
     print()
 
     # Evaluate punctuation classifier.
     punctuation_classifier = rb.PunctuationClassifier()
     print('Punctuation classifier results:')
-    evaluation.evaluate(corpus, punctuation_classifier)
+    evaluation.evaluate(corpus, punctuation_classifier, labels)
     print()
 
     # Evaluate clause classifier.
     clause_classifier = rb.ClauseClassifier()
     print('Clause classifier results:')
-    evaluation.evaluate(corpus, clause_classifier, print_classifications=False)
+    evaluation.evaluate(corpus, clause_classifier, labels)
     print()
--- speechact/evaluation.py
+++ speechact/evaluation.py
@@ -4,32 +4,58 @@ Evalaute the classifiers.
 
 import speechact.classifier.base as cb
 import speechact.corpus as corp
+import sklearn.metrics as metrics
     
 
-def evaluate(corpus: corp.Corpus, classifier: cb.Classifier, print_classifications=False):
+def evaluate(corpus: corp.Corpus, classifier: cb.Classifier, labels: list[str],
+             print_classifications=False):
     """
     Evaluate the classifier on the CoNNL-U corpus.
     """
-    correct = 0
-    total = 0
+    all_correct_labels = []
+    all_predicted_labels = []
+    # correct = 0
+    # total = 0
     for batch in corpus.batched_docs(100):
+
+        # Get the correct labels for batch.
         correct_labels = [sentence.speech_act for sentence in batch.sentences]
+        all_correct_labels += correct_labels
+
+        # Do prediction.
         classifier.classify_document(batch)
 
-        # Count correct labels.
-        for correct_label, sentence in zip(correct_labels, batch.sentences):
-            assert sentence.speech_act != None, f'Sentence does not have a speech act: {sentence.sent_id}'
+        # Get the predicted labels for batch.
+        predicted_labels = [sentence.speech_act for sentence in batch.sentences]
+        all_predicted_labels += predicted_labels
 
-            if print_classifications: 
-                print(f'Predicted: {sentence.speech_act}, correct: {correct_label}, sentence: "{sentence.text}"')
 
-            total += 1
-            if sentence.speech_act == correct_label:
-                correct += 1
+    #     # Count labels.
+    #     for correct_label, sentence in zip(correct_labels, batch.sentences):
+    #         assert sentence.speech_act != None, f'Sentence does not have a speech act: {sentence.sent_id}'
+
+    #         if print_classifications: 
+    #             print(f'Predicted: {sentence.speech_act}, correct: {correct_label}, sentence: "{sentence.text}"')
+
+    #         total += 1
+
+    #         # Count correct labels.
+    #         if sentence.speech_act == correct_label:
+    #             correct += 1
     
-    # Compute accuracy.
-    accuracy = correct / total if total > 0 else 0
+    # # Compute accuracy.
+    # accuracy = correct / total if total > 0 else 0
     
-    print('EVALUATION RESULTS')
-    print(f'Classified: {total} sentences.')
-    print(f'Accuracy: {accuracy}')
+    # print('EVALUATION RESULTS')
+    # print(f'Classified: {total} sentences.')
+    # print(f'Accuracy: {accuracy}')
+
+    report = metrics.classification_report(y_true=all_correct_labels,
+                                           y_pred=all_predicted_labels,
+                                           zero_division=0,
+                                           labels=labels
+                                           )
+    
+    print('Classification report:')
+    print(report)
+
