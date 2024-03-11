b9323a73540fbfaaf13649af5688c10206da2110
danieltufvesson <135624160+Daniel-B-Tufvesson@users.noreply.github.com>
2024-03-11 07:59:50 +0100

    Compare results with baseline MFC.

--- scripts/classify_with_rules.py
+++ scripts/classify_with_rules.py
@@ -3,10 +3,22 @@ This script classifies sentences using the rule based classfier.
 """
 from context import speechact
 import speechact.classifier.rulebased as rb
+import speechact.classifier.base as b
 import speechact.evaluation as evaluation
 import speechact.corpus as corp
 
 if __name__ == '__main__':
-    classifier = rb.RuleBasedClassifier()
     corpus = corp.Corpus('data/annotated data/dev-set.connlu.bz2')
+
+    print()
+    classifier = rb.RuleBasedClassifier()
+    print('Rule-based results:')
     evaluation.evaluate(corpus, classifier)
+    print()
+
+    most_frequent = b.MostFrequentClassifier()
+    most_frequent.train(corpora=[corpus])
+    print(f'Most frequenct class is "{most_frequent.most_common}"')
+    print('Baseline results:')
+    evaluation.evaluate(corpus, most_frequent)
+    print()
