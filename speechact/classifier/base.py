223b022a04a62258256273dc68567623eddf8494
danieltufvesson <135624160+Daniel-B-Tufvesson@users.noreply.github.com>
2024-03-11 07:45:33 +0100

    Add most frequent class classifier.

--- speechact/classifier/base.py
+++ speechact/classifier/base.py
@@ -4,19 +4,21 @@ Base code for the classifier modules.
 
 import stanza.models.common.doc as doc
 import abc
+import speechact.corpus as corp
+import collections as coll
 
 class Classifier(abc.ABC):
     """
     Base class for the speech act classifiers.
     """
 
-    @abc.abstractmethod
     def classify_document(self, document: doc.Document):
         """
         Classify all the sentences in the document. This assigns each
         sentence with a value to the 'speech_act' property.
         """
-        pass
+        for sentence in document.sentences:
+            self.classify_sentence(sentence)
 
     @abc.abstractmethod
     def classify_sentence(self, sentence: doc.Sentence):
@@ -25,3 +27,36 @@ class Classifier(abc.ABC):
         valiue to the 'speech_act' property.
         """
         pass
+
+
+class MostFrequentClassifier(Classifier):
+    """
+    The Most Frequent Class Classifier computes which speech act is most common 
+    and classifies all sentences with that. This classifier is only used as a
+    baseline.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.class_frequencies = coll.Counter()
+
+
+    def train(self, corpora: list[corp.Corpus], batch_size=100):
+        """
+        Train the classifier by computing the most frequent speech act.
+        Note that this does not reset the previous training.
+        """
+        for corpus in corpora:
+            for batch in corpus.batched_docs(batch_size):
+                for sentence in batch.sentences:
+                    assert sentence.speech_act != None, f'Sentence does not have a speech act {
+                        sentence.sent_id}'
+
+                    self.class_frequencies[sentence.speech_act] += 1
+    
+
+    def classify_sentence(self, sentence: doc.Sentence):
+        """
+        Classify the sentence with the most frequent speech act.
+        """
+        return self.class_frequencies.most_common()
