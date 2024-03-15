0a9fffce53750d6189132656599cc1d7de7250ae
danieltufvesson <135624160+Daniel-B-Tufvesson@users.noreply.github.com>
2024-03-15 15:59:05 +0100

    Annotate sentences.

--- annotater/annotater.js
+++ annotater/annotater.js
@@ -268,7 +268,7 @@ const sentences = [
 //...
 
 // Read sentences.
-const file_name = 'sents_28'
+const file_name = 'sents_30'
 const fileURL = `../data/data to annotate/real annotation/${file_name}.💬`
 const sentences = await parseSentences(fileURL, -1)
 sentences.sort(() => Math.random() - 0.5) // Shuffle sentences.
