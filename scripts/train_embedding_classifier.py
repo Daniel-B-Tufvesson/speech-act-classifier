352090e9eb90a273d6eb3b412173b4dfb7f15d0d
Daniel Tufvesson <danieltufvessonmail@gmail.com>
2024-04-08 12:49:54 +0200

    Train network on liu project computer.

--- scripts/train_embedding_classifier.py
+++ scripts/train_embedding_classifier.py
@@ -7,17 +7,22 @@ import speechact.classifier.embedding as emb
 import speechact.corpus as corp
 
 if __name__ == '__main__':
-    labels = [act.value for act in emb.SPEECH_ACTS]
+    #labels = [act.value for act in emb.SPEECH_ACTS]
 
-    #train_corpus = corp.Corpus('data/auto-annotated data/speech-acts.conllu.bz2')
-    train_corpus = corp.Corpus('data/annotated data/dev-set-sentiment-test.conllu.bz2')
+    print('Loading data...')
+    train_corpus = corp.Corpus('data/auto-annotated data/speech-acts.conllu.bz2')
+    #train_corpus = corp.Corpus('data/annotated data/dev-set-sentiment-test.conllu.bz2')
     train_data = emb.CorpusDataset(train_corpus)
 
 
     classifier = emb.EmbeddingClassifier(device='cuda')
 
+    print('Training network...')
+
     classifier.train(train_data,
-                     batch_size=32,
+                     batch_size=16,
                      num_epochs=1,
                      use_class_weights=True)
     classifier.save('models/neural/no-hidden/liu-projdator-model.pth')
+
+    print('Training complete.')
