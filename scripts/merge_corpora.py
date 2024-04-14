8c9fd854fd960a792c26c9dec3c6c822295931be
danieltufvesson <135624160+Daniel-B-Tufvesson@users.noreply.github.com>
2024-04-14 10:55:47 +0200

    Update to use sys.argv.

--- scripts/merge_corpora.py
+++ scripts/merge_corpora.py
@@ -8,13 +8,19 @@ also saved for each sentence.
 from context import speechact
 import speechact.preprocess as pre
 import speechact.corpus as corp
+import sys
 
 if __name__ == '__main__':
-    corpora = [
-        corp.Corpus('data/annotated data/dev-test-set.conllu.bz2'),
-        corp.Corpus('data/annotated data/dev-train-set.conllu.bz2')
-    ]
 
-    target_file = 'data/annotated data/dev-set.conllu.bz2'
+    # Check the number of arguments passed
+    if len(sys.argv) < 3:
+        print('Usage: python merge_corpora.py <target corpus> <corpus 1> <corpus 2> ...')
+        sys.exit(1)
+    
+    target_file = sys.argv[1]
+    corpora = [corp.Corpus(file) for file in sys.argv[2:]]
+
     pre.merge_corpora(corpora, target_file, print_progress=True, new_sent_ids=False)
     pre.print_initial_lines(target_file)
+
+# Example: python scripts/merge_corpora.py 'data/merged-set.conllu.bz2' 'data/dev-train-set.conllu.bz2' 'data/dev-test-set.conllu.bz2'
