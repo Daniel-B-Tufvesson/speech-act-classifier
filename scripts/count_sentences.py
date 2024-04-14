63ef2d588b510315714b888fa1b4ab0a5b52d0d1
danieltufvesson <135624160+Daniel-B-Tufvesson@users.noreply.github.com>
2024-04-14 13:28:44 +0200

    Update script to use sys.argv

--- scripts/count_sentences.py
+++ scripts/count_sentences.py
@@ -1,13 +1,29 @@
 """
-This script counts the sentences in each corpora and writes them to a file.
+This script counts the sentences in each corpora in a specified directory and writes them 
+to a file.
+
+Usage: python count_sentences.py <directory>
 """
+# Example: python scripts/count_sentences.py 'data'
+
 from context import speechact
 import speechact.corpus as corp
 import speechact.corpstats as cs
+import speechact.preprocess as pre
+import sys
 
 if __name__ == '__main__':
-    data_dir = 'data/tagged data'
-    corpora = corp.load_corpora_from_data_file(f'{data_dir}/data files.txt')
+
+    # Check the number of arguments passed
+    if len(sys.argv) != 2:
+        print('Usage: python count_sentences.py <directory>')
+        sys.exit(1)
+
+    data_dir = sys.argv[1]
+    
+    corpus_files = pre.list_files(data_dir, file_extension='conllu.bz2')
+    corpora = [corp.Corpus(file) for file in corpus_files]
 
     with open(f'{data_dir}/sentence counts.txt', mode='wt') as target:
         cs.write_sentence_counts(corpora, target)
+
