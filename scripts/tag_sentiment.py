a482e428f6e0017461b4af3b07366b04b9e84946
danieltufvesson <135624160+Daniel-B-Tufvesson@users.noreply.github.com>
2024-04-14 21:08:48 +0200

    Update script to use sys.argv

--- scripts/tag_sentiment.py
+++ scripts/tag_sentiment.py
@@ -1,11 +1,15 @@
 """
-This script takes a CoNNL-U corpus file and tags each sentence with its sentiment.
+This script takes a CoNLL-U corpus file and tags each sentence with its sentiment.
+
+Usage: python tag_sentiment.py <source corpus> <target corpus>
 """
+# Example: python scripts/tag_sentiment.py 'data/for-testing/dir2/dev-set-test.conllu.bz2' 'data/for-testing/dir2/dev-set-test-sentiment.conllu.bz2'
 
 from context import speechact
 import speechact.preprocess as pre
 import speechact.corpus as corp
 import bz2
+import sys
 
 def tag_corpus(bz2_source_file: str, target_file: str):
     print(f'Tagging sentences from "{bz2_source_file}" to "{target_file}".')
@@ -16,7 +20,13 @@ def tag_corpus(bz2_source_file: str, target_file: str):
         pre.tag_sentiment(corpus, target, print_progress=True)
 
 if __name__ == '__main__':
-    source_file = 'data/annotated data/test-set.conllu.bz2'
-    target_file = 'data/annotated data/test-set-sentiment.conllu.bz2'
+
+    # Check the number of arguments passed
+    if len(sys.argv) != 3:
+        print('Usage: python tag_sentiment.py <source corpus> <target corpus>')
+        sys.exit(1)
+
+    source_file = sys.argv[1]
+    target_file = sys.argv[2]
 
     tag_corpus(source_file, target_file)
