0336cdca66789a57578ca7b56e45fd60a4245b54
danieltufvesson <135624160+Daniel-B-Tufvesson@users.noreply.github.com>
2024-04-14 20:21:37 +0200

    Update script to use sys.argv

--- scripts/extract_test_subsample.py
+++ scripts/extract_test_subsample.py
@@ -1,22 +1,42 @@
 """
-Extract random sentence from a test data corpus.
+Extract random sentences from a CoNLL-U corpus, and write them to a sentence corpus.
+
+Usage: python extract_test_subsample.py <source corpus> <target sentence corpus> <N sentences>
 """
+# Example: python scripts/extract_test_subsample.py 'data/for-testing/dir1/test-set.conllu.bz2' 'data/for-testing/dir1/test-set-subsample.💬 30'
+
 from context import speechact
 import speechact.corpus as corp
 import random
+import sys
 
 if __name__ == '__main__':
-    test_corpus = corp.Corpus('data/annotated data/test-set.conllu.bz2')
-    sentences = [sent for sent in test_corpus.sentences()]
+
+    # Check the number of arguments passed
+    if len(sys.argv) != 4:
+        print('Usage: python extract_test_subsample.py <source corpus> <target sentence corpus> <N sentences>')
+        sys.exit(1)
+
+    source_file = sys.argv[1]
+    target_file = sys.argv[2]
+    n_sentences = int(sys.argv[3])
+
+    # Load and shuffle sentences from CoNLL-U corpus.
+    source_corpus = corp.Corpus(source_file)
+    sentences = [sent for sent in source_corpus.sentences()]
     random.shuffle(sentences)
 
-    target = open('data/annotated data/test-set-subsample.💬', mode='wt')
-
-    for i in range(50):
+    # Write to sentence corpus.
+    target = open(target_file, mode='wt')
+    for i in range(n_sentences):
         sentence = sentences[i]
         target.write(f'# sent_id = {sentence.sent_id}\n')
         target.write(f'# text = {sentence.text}\n')
         target.write(f'# speech_act = {sentence.speech_act}\n')
         target.write('\n')
+    
+    target.close()
+
+
         
         
