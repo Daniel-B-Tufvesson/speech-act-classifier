cffc636640bf231ac416a64e0a63b342fe2ef53d
danieltufvesson <135624160+Daniel-B-Tufvesson@users.noreply.github.com>
2024-04-14 21:02:01 +0200

    Update script to use sys.argv

--- scripts/split_corpus.py
+++ scripts/split_corpus.py
@@ -1,16 +1,27 @@
 """
-This script splits a CoNLL-U file into a training corpus file and a test corpus file.
+This script splits a CoNLL-U coprus into a training corpus file and a test corpus file.
 """
+# Example: python scripts/split_corpus.py 'data/for-testing/dir1/dev-set.conllu.bz2' 'data/for-testing/dir2' 0.8
+
 from context import speechact
 import speechact.corpus as corp
 import speechact.preprocess as pre
+import sys
+import os
 
 if __name__ == '__main__':
-    source_file = 'data/annotated data/dev-set-sentiment.conllu.bz2'
+    # Check the number of arguments passed
+    if len(sys.argv) != 4:
+        print('Usage: python split_corpus.py <corpus> <target directory> <split fraction>')
+        sys.exit(1)
+
+    source_file = sys.argv[1]
+    target_dir = sys.argv[2]
+    split_fraction = float(sys.argv[3])
+
     source_corpus = corp.Corpus(source_file)
 
-    target_dir = 'data/annotated data'
-    test_file = f'{target_dir}/{source_corpus.name}-test.conllu.bz2'
-    train_file = f'{target_dir}/{source_corpus.name}-train.conllu.bz2'
+    test_file = os.path.join(target_dir, f'{source_corpus.name}-test.conllu.bz2')
+    train_file = os.path.join(target_dir, f'{source_corpus.name}-train.conllu.bz2')
 
-    pre.split_train_test(source_corpus, test_file, train_file, 0.8, True)
+    pre.split_train_test(source_corpus, test_file, train_file, split_fraction, print_progress=True)
