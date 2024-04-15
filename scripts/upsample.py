d8c5527557a3c35d909ae0ba2c6fabf0f4c872c5
danieltufvesson <135624160+Daniel-B-Tufvesson@users.noreply.github.com>
2024-04-15 11:42:31 +0200

    Update script to use sys.argv

--- scripts/upsample.py
+++ scripts/upsample.py
@@ -2,19 +2,26 @@
 This script upsamples the corpus so that there is an equal amount of sentences 
 for each speech act label. The upsampled corpus is written to a new file.
 """
+# Example: python scripts/upsample.py 'data/for-testing/dir2/tagged/test-set.conllu.bz2'
 
 from context import speechact
 import speechact.corpus as corp
 import speechact.preprocess as pre
 import os
 import bz2
+import sys
 
 
 if __name__ == '__main__':
-    source_file = 'data/annotated data/dev-train-set.conllu.bz2'
+
+    # Check the number of arguments passed
+    if len(sys.argv) != 2:
+        print('Usage: python upsample.py <source corpus>')
+        sys.exit(1)
+
+    source_file = sys.argv[1]
+
     source_corpus = corp.Corpus(source_file)
-
-
     target_dir = os.path.dirname(source_file)
     target_file = os.path.join(target_dir, f'{source_corpus.name}-upsampled.conllu.bz2')
 
